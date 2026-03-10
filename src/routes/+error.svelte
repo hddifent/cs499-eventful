69a414d4cecf2659ceb1d8161b3708a3d1d579d4
hddifent <preukwork@gmail.com>
2026-03-10 21:29:50 +0700

    Error landing page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<div class="py-8">
+	<div class="space-y-2 px-8">
+		<div class="space-x-2">
+			<span class="text-5xl font-bold">{page.status}</span>
+			{#if page.error}
+				<span class="text-2xl">{page.error.message}</span>
+			{/if}
+		</div>
+		<span>Ah, shuck... Let's try that again, shall we?</span>
+	</div>
+</div>
