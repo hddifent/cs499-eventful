6f5d80a6907cf55198dc8ce6ba0317e5fff00d56
hddifent <preukwork@gmail.com>
2026-03-13 16:54:55 +0700

    Add placeholders for account page.

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -1,15 +1,23 @@
 <script lang="ts">
 	import MdiAccount from 'virtual:icons/mdi/account';
 
-	import { Avatar } from 'melt/builders';
+	import { Avatar, Tabs } from 'melt/builders';
 	import type { PageServerData } from './$types';
 
 	const { data }: { data: PageServerData } = $props();
 
 	const avatar = new Avatar({ src: () => data.user.pfpUrl });
+
+	const tabNames = ['General', 'My Organizer Groups', 'My Events', 'Settings'] as const;
+	type TabId = (typeof tabNames)[number];
+	const accountTabs = new Tabs<TabId>({
+		value: tabNames[0],
+		orientation: 'vertical'
+	});
 </script>
 
 <div class="p-8">
+	<!-- Flex User Profile -->
 	<div class="flex items-center gap-x-4">
 		<!-- Avatar -->
 		<div class="h-24 w-24 rounded-full bg-secondary text-bg">
@@ -23,6 +31,41 @@
 		<div class="space-y-2">
 			<div class="text-5xl font-bold">{data.user.displayName}</div>
 			<div class="text-2xl">@{data.user.username}</div>
+		</div>
+	</div>
+
+	<span class="flex w-full items-center justify-center py-4">
+		<hr class="w-full border border-fg/50" />
+	</span>
+
+	<!-- Menu -->
+	<div class="flex space-x-4">
+		<!-- Tab Menu -->
+		<div {...accountTabs.triggerList} class="w-min space-y-1">
+			{#each tabNames as t (t)}
+				<button
+					class="group w-full bg-transparent font-bold text-ellipsis whitespace-nowrap transition outline-none"
+					{...accountTabs.getTrigger(t)}
+				>
+					<div
+						class="overflow-clip rounded-lg px-4 py-2 text-left transition group-data-active:bg-primary group-[&:not([data-active]):hover]:bg-primary/50"
+					>
+						{t}
+					</div>
+				</button>
+			{/each}
+		</div>
+
+		<!-- Divider -->
+		<span class="flex h-auto w-0 border border-fg/50"></span>
+
+		<!-- Tab Contents -->
+		<div class="mt-2">
+			{#each tabNames as t (t)}
+				<div {...accountTabs.getContent(t)}>
+					{t} Contents
+				</div>
+			{/each}
 		</div>
 	</div>
 </div>
