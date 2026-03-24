aec74043424d56feff2bacd8cfcb8ba2f94ffab6
hddifent <preukwork@gmail.com>
2026-03-24 16:48:18 +0700

    Zod refactorization

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,35 +1,18 @@
 import { fail, redirect } from '@sveltejs/kit';
 import type { Actions } from './$types';
 import { z } from 'zod';
+import {
+	zDisplayNameLikeField,
+	zPasswordLikeField,
+	zUsernameLikeField
+} from '$lib/snippets/zodFields';
 
 const registerSchema = z
 	.object({
 		email: z.email({ message: 'Invalid email address.' }),
-
-		username: z
-			.string()
-			.min(1, { message: 'Username cannot be empty.' })
-			.max(32, { message: 'Username must not exceed 32 characters.' })
-			.regex(/^[a-zA-Z]/, { message: 'Username must start with a letter.' })
-			.regex(/^(?!.*[_.]{2})/, {
-				message: 'Username cannot have consecutive underscores or periods.'
-			})
-			.regex(/^[a-zA-Z0-9_.]+$/, {
-				message: 'Username can only contain letters, numbers, underscores, or periods.'
-			}),
-
-		displayName: z
-			.string()
-			.trim()
-			.min(1, { message: 'Display name cannot be empty.' })
-			.max(100, { message: 'Display name must not exceed 100 characters.' })
-			.regex(/^[a-zA-Z0-9_. -]+$/, { message: 'Display name contains invalid characters.' }),
-
-		password: z
-			.string()
-			.min(8, { message: 'Password must be at least 8 characters.' })
-			.max(64, { message: 'Password must not exceed 64 characters.' }),
-
+		username: zUsernameLikeField(),
+		displayName: zDisplayNameLikeField(),
+		password: zPasswordLikeField(),
 		confirmPassword: z.string()
 	})
 	.refine((data) => data.password === data.confirmPassword, {
